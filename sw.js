1226d1ae87c67503a31c5abb556ad8388aeaf05e
oneplus-lms <oneplusul@gmail.com>
2026-04-16 08:15:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LabFlow Service Worker — v19.50
-const CACHE_NAME = 'labflow-v19.50';
+// LabFlow Service Worker — v19.55
+const CACHE_NAME = 'labflow-v19.55';
 
 const PRECACHE = [
   '/oneplus-lms/index.html',

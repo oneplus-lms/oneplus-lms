59e7a4ecabd499aed66ae5f72e570c804c69e1ab
oneplus-lms <oneplus-lms@users.noreply.github.com>
2026-05-13 06:45:08 +0000

    deploy: d3cf2702986890a585096eed5a4027e078ca6497

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LabFlow Service Worker — labflow-v21.19
-const CACHE_NAME = 'labflow-v21.19';
+// LabFlow Service Worker — labflow-v21.23
+const CACHE_NAME = 'labflow-v21.23';
 
 const PRECACHE = [
   '/oneplus-lms/manifest.json',
